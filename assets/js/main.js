52fbc549445bbc0bcccc55ecffe4838129e09cc0
Tejaswini <9549564+tejaswinir@users.noreply.github.com>
2025-02-03 18:01:13 +0530

    home page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,18 @@
 window.addEventListener("scroll", function () {
-    let navbar = document.getElementById("navbar");
-    if (window.scrollY > 50) {
+  let navbar = document.getElementById("navbar");
+  if (window.scrollY > 50) {
       navbar.classList.add("shrink");
-    } else {
+  } else {
       navbar.classList.remove("shrink");
-    }
-  });
+  }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  if (typeof Marquee3k !== "undefined") {
+      Marquee3k.init();
+  } else {
+      console.error("Marquee3k is not loaded.");
+  }
+
+  AOS.init();
+});
