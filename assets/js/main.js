86339831abaf5a175230294e425c480a59fec7b0
Tejaswini <9549564+tejaswinir@users.noreply.github.com>
2025-02-04 14:37:36 +0530

    svg animation for milestone

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,31 @@
-window.addEventListener("scroll", function () {
-  let navbar = document.getElementById("navbar");
-  if (window.scrollY > 50) {
-      navbar.classList.add("shrink");
-  } else {
-      navbar.classList.remove("shrink");
-  }
-});
 
 document.addEventListener("DOMContentLoaded", function () {
-  if (typeof Marquee3k !== "undefined") {
-      Marquee3k.init();
-  } else {
-      console.error("Marquee3k is not loaded.");
-  }
+    let parentContainer = document.querySelector(".forge-milestone-md");
+    
+    let svgElement = parentContainer.getElementsByTagName("svg")
 
+    function checkVisibility(){
+        let rect = parentContainer.getBoundingClientRect();
+
+        let isVisible = rect.top < window.innerHeight && rect.bottom >= 0;
+
+        if (isVisible) {
+            svgElement[0].classList.add("active");
+        }
+        else{ 
+          svgElement[0].classList.remove("active");
+        }
+    }
+
+    window.addEventListener("scroll", function () {
+      let navbar = document.getElementById("navbar");
+      if (window.scrollY > 50) {
+          navbar.classList.add("shrink");
+      } else {
+          navbar.classList.remove("shrink");
+      }
+    });
+  window.addEventListener("scroll", checkVisibility);
+  checkVisibility();  // Run once on page load
   AOS.init();
 });
