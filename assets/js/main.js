9dad325de479156447e20048423c569071c92e85
Tejaswini <9549564+tejaswinir@users.noreply.github.com>
2025-02-05 18:43:24 +0530

    Seperated the js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,5 @@
 
 document.addEventListener("DOMContentLoaded", function () {
-    let parentContainer = document.querySelector(".forge-milestone-md");
-    if(parentContainer){
-    var svgElement = parentContainer.getElementsByTagName("svg")
-    }
-    function checkVisibility(){
-        let rect = parentContainer.getBoundingClientRect();
-
-        let isVisible = rect.top < window.innerHeight && rect.bottom >= 0;
-
-        if (isVisible) {
-            svgElement[0].classList.add("active");
-        }
-        else{ 
-          svgElement[0].classList.remove("active");
-        }
-    }
-
     window.addEventListener("scroll", function () {
       let navbar = document.getElementById("navbar");
       if (window.scrollY > 50) {
@@ -25,7 +8,6 @@ document.addEventListener("DOMContentLoaded", function () {
           navbar.classList.remove("shrink");
       }
     });
-  window.addEventListener("scroll", checkVisibility);
   // Run once on page load
   AOS.init();
 });
